23bd45c9e916b5a711a3cc824b4b7b4f8de2894b
Magreus <matheussilvarocha013@gmail.com>
2023-08-22 11:04:46 -0300

    att

---
+++ JSUC6-introducao/jsjogos.js
@@ -0,0 +1,8 @@
+let inicial = 150;
+let bonus = 80;
+let perdidos = 30;
+
+var final = inicial + bonus - perdidos;
+
+console.log("parabens sua pontuação final é ", final,
+"você perdeu ", perdidos);
